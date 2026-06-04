e6ce92e5d4b6f67dab5b0913bba8e458b58e9c0d
Paulo Mendes <paulo.h.mendes25@gmail.com>
2026-06-04 01:08:56 -0300

    feat(api): add CreatePatientDto

---
+++ apps/api/src/patients/dto/create-patient.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsString, MaxLength } from 'class-validator';
+import { UpdatePatientDto } from './update-patient.dto';
+
+// Inherits all the optional clinical fields from UpdatePatientDto and adds the
+// two fields required to create + invite a patient.
+export class CreatePatientDto extends UpdatePatientDto {
+  @IsString()
+  @MaxLength(200)
+  name!: string;
+
+  @IsEmail()
+  email!: string;
+}
